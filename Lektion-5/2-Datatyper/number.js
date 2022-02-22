811d877c27511310e33997c11a1a6af57b634c4f
Joakim <wahlstrom.joakim@gmail.com>
2022-02-22 10:43:54 +0100

    lektion 5

---
+++ Lektion-5/2-Datatyper/number.js
@@ -0,0 +1,47 @@
+/* 
+  NUMBERS
+  -----------------------------------------------------------------
+
+  +   lägg till
+  -   ta bort
+  *   gångra
+  **  upphöjt i
+  /   dela
+  ++  öka med 1
+  --  minska med 1
+  %   division remainder
+
+  =   lika med
+  +=  lägg till
+  -=  ta bort från
+  *=  gångra och tilldela
+  /=  dela och tilldela
+  %=  division remainder
+
+*/
+
+
+let nr1 = 10;
+let nr2 = 2.5;
+
+// console.log(nr1, nr2);
+
+// console.log(nr1 + nr2);
+// console.log(nr1 - nr2);
+
+nr1 = nr1 + 1;
+nr1 += 1;
+nr1 ++;
+
+nr1 = nr1 - 1;
+nr1 -= 1;
+nr1 --;
+
+
+// console.log(nr1)
+console.log(nr1 * nr2);
+console.log(nr1 ** nr2);
+console.log(nr1 % 4);
+
+console.log(6 % 2)
+console.log(7 % 2)
--- Lektion-5/2-Datatyper/script.js
+++ Lektion-5/2-Datatyper/script.js
@@ -7,5 +7,109 @@
     null          =   tomt, sätts manuellt
     undefined     =   tomt, har en blivit definerat. (automatiskt)
     Object        =   ett objekt/klass innehåller ett eller flera värden, Array är en objekt typ, function är också en slags objekt
-    (Array)         =   objekt, lista
+    (Array)       =   objekt, lista
 */
+
+
+
+/* 
+  Variabler
+
+  variabel = någonting som vi lagrar information i
+
+  camelCase   =   javaScript, firstName, lastName, email, postalCode
+  _private    =   _firstName = 'Joakim'
+
+
+
+
+  Deklarera variabler
+  ------------------------------------------------------------------
+
+  name = 'Joakim' - global variabel
+  var name = 'Joakim'
+  let name = 'Joakim'
+  const name = 'Joakim'
+
+
+  var = global variabel - accessbar överallt i koden
+  let = lokal variabel - accessbar bara inom det angivna området
+  const = konstant, kan inte skrivas över (undantag finns)
+
+
+  C# exempel.
+    string name = "Joakim";
+    int age = 35;
+
+  JS exempel.
+    let name = 'Joakim';
+    let age = 35;
+
+*/
+
+
+/* 
+  Strings:
+    let name = "Joakim"
+    let name = 'Joakim'
+    let name = `Joakim`  (shift + ´ , tryck mellanslag)
+*/
+
+
+// let name = 'Joakim';
+// let age = 35;
+
+// console.log(name, age)
+
+
+// let withoutPlaceholders = 'Mitt namn är ' + name + ' och jag är ' + age + ' år gammal.'
+
+// Template string
+// let withPlaceholders = `Mitt namn är ${name} och jag är ${age} år gammal`
+
+// console.log(withoutPlaceholders)
+// console.log(withPlaceholders)
+
+
+var varName = 'var Joakim';
+let letName = 'let Joakim';
+const constName = 'const Joakim';
+
+
+// console.log(varName)
+// console.log(letName)
+// console.log(constName)
+
+varName = 'var John'
+letName = 32
+// constName = 'const John'
+
+// console.log(varName)
+// console.log(letName)
+
+
+let firstName = 'Joakim';
+let lastName = 'Wahlström'
+
+let myName = firstName + ' ' + lastName
+
+console.log(myName)
+
+
+console.log(myName.length);
+console.log(myName[0]);
+console.log(myName[1]);
+console.log(myName.indexOf('a'))
+console.log(myName.lastIndexOf('a'))
+
+console.log(myName.toUpperCase())
+console.log(myName.toLowerCase())
+
+let names = myName.split(' ')
+
+console.log(names)
+let _fName = names[0]
+
+console.log(_fName)
+
+console.log(`Hej ${_fName}, Välkommen tillbaka`)
