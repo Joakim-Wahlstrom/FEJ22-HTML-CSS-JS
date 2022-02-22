3b92ffb222bdb1a14e91e2f2bf8713c1a8adb7ca
Joakim <wahlstrom.joakim@gmail.com>
2022-02-22 09:41:43 +0100

    lektion 5

---
+++ Lektion-5/1-hello_world/script.js
@@ -0,0 +1,33 @@
+// alert('Hello world')
+
+// en kommentar kan skrivas såhär (ctrl + ')
+
+/*   
+  en kommentar kan skrivas såhär
+  om det är flera rader           (shift + alt + a)
+*/
+
+/* 
+  Högnivå språk (mellan användaren och systemet)
+  ---------------------------------------------------------------
+  JavaScript
+  Python
+  C#
+  Java
+
+  Lågnivå språk (mellan systemet och hårdvara)
+  ---------------------------------------------------------------
+  C++
+  C
+  Mikrokod
+
+*/
+
+//skriva ut på hemsidan
+document.body.innerText = 'Hello world';
+
+// skriva ut i consolen
+console.log('Här skriver vi ut någonting i konsolen');
+console.warn('Detta är en varning');
+console.error('Detta är ett felmeddelande');
+
---
+++ Lektion-5/2-Datatyper/script.js
@@ -0,0 +1,11 @@
+/* 
+    Datatyper
+    -----------------------------------------
+    String        =   Text
+    Number        =   Tal, decimaltal, stora tal, små tal
+    Boolean       =   true/false
+    null          =   tomt, sätts manuellt
+    undefined     =   tomt, har en blivit definerat. (automatiskt)
+    Object        =   ett objekt/klass innehåller ett eller flera värden, Array är en objekt typ, function är också en slags objekt
+    (Array)         =   objekt, lista
+*/
