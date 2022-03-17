7e7fa7a1b9d5fc6b140771cf54fc59fd64d53c18
Joakim <wahlstrom.joakim@gmail.com>
2022-03-17 13:43:59 +0100

    Lektion 9

--- Lektion-9/1-todo/script.js
+++ Lektion-9/1-todo/script.js
@@ -24,16 +24,57 @@ const listTodos = () => {
 
 
 const createTodoElement = (todo, parent, placement, isNew) => {
+  let title = todo.title.replace('<', '&lt').replace('>', '&gt')
   parent.insertAdjacentHTML(placement, `
   <div id="todo_${todo.id}" class="border-bottom py-2 ${isNew ? 'slide-in' : ''}">
     <div class="d-flex justify-content-between align-items-center">
-      <p id="title_${todo.id}" class="h4 m-0 todo-title ${todo.completed ? 'complete' : ''}">${todo.title}</p>
+      <p id="title_${todo.id}" class="h4 m-0 todo-title ${todo.completed ? 'complete' : ''}">${title}</p>
       <button id="delete_${todo.id}" class="btn btn-danger btn-sm">X</button>
     </div>
   </div>`)
 
   addRemoveOnClick(todo)
+  addToggleComplete(todo)
+}
 
+const addToggleComplete = (todo) => {
+  document.querySelector(`#title_${todo.id}`).addEventListener('click', () => {
+    // fetch(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, {
+    //   method: 'PUT',
+    //   body: JSON.stringify({
+    //     id: todo.id,
+    //     completed: !todo.completed,
+    //     title: todo.title,
+    //     userId: 1
+    //   }),
+    //   headers: {
+    //     'Content-type': 'application/json; charset=UTF-8',
+    //   },
+    // })
+    // .then(res => {
+    //   if(!res.ok) {
+    //     error.innerText = 'Something went wrong'
+    //     error.classList.remove('d-none')
+    //     return
+    //   }
+    //   error.classList.add('d-none')
+    //   console.log(res)
+    //   return res.json()
+    // })
+    // .then(data => {
+    //   console.log(data)
+    //   const _todo = document.querySelector('#todo_' + data.id)
+    //   createTodoElement(data, _todo, 'beforebegin', false)
+    //   _todo.remove()
+    // })
+    // .catch(err => console.log(err))
+
+    todo.completed = !todo.completed
+    console.log(todos)
+    const todoElement = document.querySelector('#todo_' + todo.id)
+    createTodoElement(todo, todoElement, 'beforebegin', false)
+    todoElement.remove()
+  })
 }
 
 const addRemoveOnClick = (todo) => {
@@ -81,7 +122,8 @@ const createTodo = async (title) => {
     },
     body: JSON.stringify({
       title,
-      completed: false
+      completed: false,
+      userId: 1
     })
   })
 
@@ -94,8 +136,8 @@ const createTodo = async (title) => {
 
 
   const todo = await res.json()
-  todo.id = Date.now()
-  
+  // todo.id = Date.now()
+  console.log(todo)
   todos.push(todo)
   // output.insertAdjacentHTML('beforeend', createTodoElement(todo))
   createTodoElement(todo, output, 'beforeend', true)
